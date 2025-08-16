5ab28343490dee486686dce0be9cef8d016a4935
josemiguelhyb <josemiguelhyb@gmail.com>
2025-08-16 20:49:31 +0200

    Añade .gitignore y primeros archivos del proyecto

---
+++ src/main/java/com/josemiguelhyb/citas_sanitarias/CitasSanitariasApplication.java
@@ -0,0 +1,13 @@
+package com.josemiguelhyb.citas_sanitarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasSanitariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasSanitariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemiguelhyb/citas_sanitarias/CitasSanitariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemiguelhyb.citas_sanitarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasSanitariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
