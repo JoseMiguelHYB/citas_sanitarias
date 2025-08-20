91fc19c6e9b432ec4fbe4360b0327ce24bb0563f
josemiguelhyb <josemiguelhyb@gmail.com>
2025-08-20 08:57:20 +0200

    integrando swagger open api

---
+++ src/main/java/com/josemiguelhyb/citas_sanitarias/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.josemiguelhyb.citas_sanitarias.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Citas Sanitarias")
+                        .version("1.0")
+                        .description("Documentación de la API del sistema de gestión de citas sanitarias"));
+    }
+}
